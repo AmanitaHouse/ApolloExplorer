0f4694384ea160f0c1978809beb7ca5209129b4a
Willem Drijver <willemdrijver@mac.com>
2026-04-07 17:11:50 +0200

    Update MacOS Build

--- build_macos_client.sh
+++ build_macos_client.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+brew install qt5
+
+rm -r -f .qmake.stash
 
 clear
 /opt/homebrew/opt/qt@5/bin/qmake -recursive
@@ -6,4 +9,7 @@ make clean
 make -j16
 make clean
 
-cp ApolloExplorerPC/icons/ApolloExplorer.icns ApolloExplorerPC/ApolloExplorer.app/Contents/Resources/
+cp ApolloExplorerPC/icons/ApolloExplorer.icns ApolloExplorerPC/ApolloExplorer.app/Contents/Resources/ 
+
+rm -r -f .qmake.stash Makefile ApolloExplorerPC/Makefile ApolloExplorerPC/qmake_qmake_qm_files.qrc
+rm -r -f acp/Makefile AmigaIconReader/Makefile 
